ea789e8e5ac57a9a896543af61b3273ebd2cf3be
Esmatt123 <esmatt98@hotmail.com>
2025-01-25 19:40:55 +0100

    running tests attempt #3

--- StudyProductivityApp.Application/obj/Debug/net8.0/StudyProductivityApp.Application.AssemblyInfo.cs
+++ StudyProductivityApp.Application/obj/Debug/net8.0/StudyProductivityApp.Application.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("StudyProductivityApp.Application")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+85fdd220bae252dee529baea21b8d26057b48067")]
 [assembly: System.Reflection.AssemblyProductAttribute("StudyProductivityApp.Application")]
 [assembly: System.Reflection.AssemblyTitleAttribute("StudyProductivityApp.Application")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
