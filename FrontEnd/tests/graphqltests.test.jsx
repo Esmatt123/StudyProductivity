b7268a1335d1f0b6acaceacd97b189d3fbb90bb7
Esmatt123 <esmatt98@hotmail.com>
2025-01-25 21:01:44 +0100

    running tests attempt #6

--- FrontEnd/tests/graphqltests.test.jsx
+++ FrontEnd/tests/graphqltests.test.jsx
@@ -77,7 +77,7 @@ import { ApolloClient, InMemoryCache } from '@apollo/client';
 
 
 
-const GRAPHQL_ENDPOINT = "studyproductivityapp-backend-bpc4cxfhctgqgeev.polandcentral-01.azurewebsites.net";
+const GRAPHQL_ENDPOINT = "studyproductivityapp-backend-bpc4cxfhctgqgeev.polandcentral-01.azurewebsites.net/graphql";
 
 // Create a mock client with MockLink
 function createMockClient(mocks) {
